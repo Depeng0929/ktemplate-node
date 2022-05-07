b845652ce263226212c2e1c7d7bd0d9563b548a3
depeng.kong <15365182986@163.com>
2022-05-07 23:47:47 +0800

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,19 @@ import { UserModule } from 'src/modules/user/user.module';
 import { MailerModule } from '@nest-modules/mailer';
 import { ConfigModule, ConfigService } from 'nestjs-config';
 import { resolve } from 'path';
+import { StatusMonitorModule } from 'nest-status-monitor';
+import StatusMonitorConfig from './config/statusMonitor';
 
 @Module({
   imports: [
     ConfigModule.load(resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
-    ,
-    UserModule,
+    StatusMonitorModule.setUp(StatusMonitorConfig),
     MailerModule.forRootAsync({
       useFactory: (config: ConfigService) => config.get('email'),
       inject: [ConfigService],
     }),
+
+    UserModule,
   ],
   controllers: [],
   providers: [],
--- src/config/email.ts
+++ src/config/email.ts
@@ -2,24 +2,22 @@ import { resolve } from 'path';
 import { PugAdapter } from '@nest-modules/mailer';
 
 export default {
-  useFactory: () => ({
-    transport: {
-      host: 'smtp.163.com',
-      port: '465',
-      auth: {
-        user: '15365182986@163.com',
-        pass: '',
-      },
+  transport: {
+    host: 'smtp.163.com',
+    port: '465',
+    auth: {
+      user: '15365182986@163.com',
+      pass: '',
     },
-    defaults: {
-      form: '15365182986@163.com',
+  },
+  defaults: {
+    form: '15365182986@163.com',
+  },
+  template: {
+    dir: resolve(__dirname, '../templates/email'),
+    adapter: new PugAdapter(),
+    options: {
+      strict: true,
     },
-    template: {
-      dir: resolve(__dirname, '../templates/email'),
-      adapter: new PugAdapter(),
-      options: {
-        strict: true,
-      },
-    },
-  }),
+  },
 };
